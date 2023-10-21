4dced0f30723de6c6adb81f634f0b1aa2bb9bfa2
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-10-21 11:52:58 +0300

    Add spaces for cleaner code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 
+from passlib.context import CryptContext
 from fastapi import FastAPI, Depends
 from fastapi.responses import RedirectResponse
 from fastapi.security import OAuth2PasswordBearer
@@ -9,9 +10,12 @@ from config.database import create_db
 from fastapi.middleware.cors import CORSMiddleware
 from routes import user, auth
 
+
 app = FastAPI(debug=True, title='NeforStore')
 app_name = 'main:app'
 oAuth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+
 app.add_middleware(CORSMiddleware,
         allow_origins=['*'],
         allow_credentials=True,
@@ -23,9 +27,11 @@ app.add_middleware(CORSMiddleware,
 app.include_router(auth.auth)
 app.include_router(user.user)
 
+
 @app.get('/token')
 async def test_token(token: Annotated[str, Depends(oAuth2_scheme)]):
         return {'token': token }
+
 
 @app.get('/')
 def index():
