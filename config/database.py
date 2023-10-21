2b31c9de50971fb9e768a699c55dc8544272b865
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-10-21 11:50:54 +0300

    Update user mode. Add user input model

--- config/database.py
+++ config/database.py
@@ -1,7 +1,6 @@
 from typing import Optional
-from pydantic import EmailStr
-from sqlmodel import Field, SQLModel, create_engine
-from config.config import Config
+from pydantic import EmailStr, BaseModel
+from sqlmodel import Field, SQLModel, Session, create_engine
 
 
 class User(SQLModel, table=True):
@@ -9,15 +8,21 @@ class User(SQLModel, table=True):
     first_name: str = Field(alias='firstName')
     last_name: str = Field(alias='lastName')
     birth_date: str = Field(alias='birthday')
+    email: EmailStr = Field(unique=True)
     # adress: str
     # role: str
     # phone_number: str
-    email: EmailStr
     password: str
     # password_salt: str
 
 
+class UserInput(BaseModel):
+    email: EmailStr
+    password: str
+
+
 engine = create_engine('sqlite:///nefor.db')
+session = Session(bind=engine)
+
 def create_db() -> None:
     SQLModel.metadata.create_all(engine)
-    
