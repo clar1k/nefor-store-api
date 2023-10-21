f674a9e76980e88f02f4e6e45e3110b76b8cecd2
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-10-21 11:56:30 +0300

    Add exception catch, update hash function

--- routes/user.py
+++ routes/user.py
@@ -2,21 +2,25 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from config.database import engine, User
 from sqlmodel import Session
-from passlib.hash import pbkdf2_sha256
-from sqlite3 import IntegrityError
+from passwords.password import get_password_hash
+from sqlalchemy.exc import IntegrityError
+
 user = APIRouter(prefix='/user', tags=['User'])
 
 
 @user.post('/')
 def create_user(new_user: User) -> JSONResponse:
-    new_user.password = pbkdf2_sha256.hash(new_user.password)
+    new_user.password = get_password_hash(new_user.password)
+    
     with Session(engine) as session:
         try:
             session.add(new_user)
             session.commit()
-        except IntegrityError:
+        except IntegrityError as exception:
+            print('Exception generated :\n', exception)
             session.rollback()
-            return JSONResponse({'msg': 'User is already registered'}, 401)        
+            return JSONResponse({'msg': 'User is already registered'}, 401)
+    
     return JSONResponse({'msg': 'Success'}, 201)
 
 
