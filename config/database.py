14628db5da479625ca0a4d6cb0cdfbb5b9d73e37
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-12-01 10:26:12 +0200

    fix: update db url

--- config/database.py
+++ config/database.py
@@ -1,9 +1,9 @@
 import logging
-
+from config.config import Config
 from sqlmodel import SQLModel, create_engine
 
 logging.basicConfig(filename='sql.log', level=logging.INFO)
-engine = create_engine('sqlite:///nefor-store.db')
+engine = create_engine(Config.DATABASE_URL)
 
 
 def create_db_and_tables() -> None:
